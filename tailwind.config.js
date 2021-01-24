46e4f154c250780d1fecd4c53d135a5b05dfca75
Nishino0719 <nishino0719@icloud.com>
2021-01-24 12:24:10 +0900

    :package: add tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "media", // 'media' or 'class'
-  theme: { extend: { colors: { "accent-1": "#333" } } },
-  variants: { extend: {} },
-  plugins: [],
-};
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media', // 'media' or 'class'
+  theme: { extend: { colors: { 'accent-1': '#333' } } },
+  variants: {
+    extend: {
+      zIndex: ['hover', 'active'],
+      maxHeight: ['hover', 'focus']
+    }
+  },
+  plugins: []
+}
